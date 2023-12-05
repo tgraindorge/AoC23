a27cfdb8458438476ef7d23e9b0b378d8f5d2a9f
Thomas <tecnisfr@gmail.com>
2023-12-06 00:46:22 +0100

    day3

--- Day3/script.py
+++ Day3/script.py
@@ -1,3 +1,64 @@
 import re
-file = open('input.txt', 'r')
-Lines = file.readlines()
+
+def main():
+    file = open('input2.txt', 'r')
+    Lines = file.readlines()
+
+    Total = 0
+    currLineNum = 0
+    for line in Lines:
+        if currLineNum == 0:
+            prevLine = None
+        else:
+            prevLine = Lines[currLineNum-1]
+        if currLineNum+1 == len(Lines):
+            nextLine = None
+        else:
+            nextLine = Lines[currLineNum+1]
+
+        for match in re.finditer("([0-9]+)", line):
+            #print("checking " + match.group())
+            #if prevLine: print("prevLine " + prevLine)
+            #if nextLine: print("nextLine " + nextLine)
+            if isValidNumber(match.start(), (match.end() - match.start()), prevLine, line, nextLine):
+                Total += int(match.group())
+                print("  >> " + match.group() + " is valid")
+        currLineNum += 1
+    print(Total)
+
+
+
+
+def symbolPresent(data : str, startPosition : int, length: int) ->bool:
+    overflow = startPosition + length - (len(data)-1)
+    correctedLength = length
+    if overflow > 0:
+        correctedLength -= overflow
+    if startPosition < 0:
+        startPosition = 0
+        length-=1
+    #print("corrected length: " + str(correctedLength) + " - overflow: " + str(overflow))
+    if re.search(r"^.{" + str(startPosition) + "}(?![.0-9]{" + str(correctedLength) + "})",data):
+        return True
+    else:
+        return False
+
+def isValidNumber(startPosition : int, length: int, prevLine: str, curLine : str, nextLine : str) ->bool:
+    if prevLine and symbolPresent(prevLine, startPosition-1, length+2):
+        #print("++ Prev Line / position: " + str(startPosition-1) + " / length: " + str(length+2) + " / len(line): " + str(len(prevLine)-1))
+        print("++ Prev Line")
+        return True
+    if startPosition > 0 and symbolPresent(curLine, startPosition-1, 1):
+        print("++ before")
+        return True
+    if (startPosition+length) < (len(curLine)-1) and symbolPresent(curLine, startPosition+length, 1):
+        #print("++ after / position: " + str(startPosition) + " / length: " + str(length) + " / len(line): " + str(len(curLine)-1))
+        print("++ after")
+        return True
+    if nextLine and symbolPresent(nextLine, startPosition-1, length+2):
+        print("++ Next Line")
+        return True
+    return False
+
+    
+main()
