89d2976741b409c5d142e45d18ec51eb0e37b895
LE RU-GRAINDORGE Thomas <TO155825@eu.airbus.corp>
2023-12-07 15:44:56 +0100

    Day 4 Part 2

--- Day4/script.py
+++ Day4/script.py
@@ -1,27 +1,38 @@
 import re
 
-debug = False
-
 def main():
     file = open('input.txt', 'r')
     Lines = file.readlines()
 
     Total = 0
+
+    cardCopies = list()
+    for i in range(len(Lines)):
+        cardCopies.append(1)
+    lineKey = 0
     for line in Lines:
         winNums = list()
-        myNums = list()
         cardPoints = 0
         cardsInfo = line.split(":")[1]
         for num in re.finditer(r"([0-9]+)(?!:)", cardsInfo.split("|")[0]):
             winNums.append(int(num.group()))
+        winNumsCount = 0
         for num in re.finditer(r"([0-9]+)(?!:)", cardsInfo.split("|")[1]):
             if int(num.group()) in winNums:
-                if debug : print("win Num: " + num.group())
+                winNumsCount += 1
                 if cardPoints == 0:
                     cardPoints=1
                 else:
                     cardPoints *= 2
+                
+        for i in range(winNumsCount):
+            cardCopies[lineKey + 1 + i] += cardCopies[lineKey]
         Total+=cardPoints
-    print(Total)
+        lineKey += 1
+    total_part2 = 0
+    for i in range(len(cardCopies)):
+        total_part2 += cardCopies[i]
+    print("Part 1: " + str(Total))
+    print("Part 2: " + str(total_part2))
 
 main()
