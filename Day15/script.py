6586bc76a9a032e3d0ca4fbfe4291a9372c44037
LE RU-GRAINDORGE Thomas <TO155825@eu.airbus.corp>
2023-12-19 14:40:45 +0100

    Day15

--- Day15/script.py
+++ Day15/script.py
@@ -1,32 +1,27 @@
 def main():
-    with open('input2.txt', 'r') as f:
+    with open('input.txt', 'r') as f:
         data = [s for s in f.read().split(",")]
         total = 0
         for s in data:
-            total += holiday(s)
+            total += holidayHash(s)
         print("Part 1: " + str(total))
 
         boxes = []
         for i in range(256):
             boxes.append(Box())
-        
         for s in data:
-            print("Label: " + s.split("=")[0] + " | box: " + str(holiday(s.split("=")[0])) + " | focale: " + s[-1])
             if "=" in s:
-                boxes[holiday(s.split("=")[0])].addLens(s)
+                boxes[holidayHash(s.split("=")[0])].addLens(s)
             else:
-                boxes[holiday(s.split("-")[0])].removeLens(s.split("-")[0])
+                boxes[holidayHash(s.split("-")[0])].removeLens(s.split("-")[0])
 
         total_p2 = 0
-        
         for i, b in enumerate(boxes):
             total_p2 += b.getTotal(i+1)
-            if len(b.lenses) > 0:
-                print("Box #" + str(i+1) + " > " + str(b.getTotal(i+1)))
         
         print("Part 2: " + str(total_p2))
 
-def holiday(str):
+def holidayHash(str):
     seq = 0
     for c in str:
         seq += ord(c)
@@ -47,30 +42,25 @@ class Box:
             if l.label == _label:
                 return i
         return 99999
-    
+
     def addLens(self, _str):
         newLens = Lens(_str)
-        existingLensIndex = self.hasLens(newLens.label)
-        if existingLensIndex != 99999:
-            self.lenses[existingLensIndex] = newLens
-        else:
-            self.lenses.append(newLens)
+        for l in self.lenses:
+            if l.label == newLens.label:
+                l.val = newLens.val
+                return
+        self.lenses.append(newLens)
 
     def removeLens(self, _label):
-        lensIndex = 99999
-        for i, l in enumerate(self.lenses):
-            if l.label == _label:
-                lensIndex = i
+        lensIndex = self.hasLens(_label)
         if lensIndex != 99999:
             self.lenses.pop(lensIndex)
 
     def getTotal(self, _boxIndex):
         total = 0
         for i, l in enumerate(self.lenses):
-            print(l.label)
-            total += _boxIndex * i * l.val
+            total += _boxIndex * (i+1) * l.val
         return total
-
 
 if __name__ == "__main__":
     main()
