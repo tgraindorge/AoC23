ad4fb8e24883a4b24e51e6efc212b0dd6b0c547c
Thomas <tecnisfr@gmail.com>
2023-12-31 02:37:50 +0100

    Day18

---
+++ Day18/script.py
@@ -0,0 +1,31 @@
+from shapely import *
+
+def main():
+    file = open('input.txt', 'r').read()
+    instr = [[ln.split()[0], int(ln.split()[1]), ln.split()[2][2:8]] for ln in file.splitlines()]
+    instr_p2 = [[ln.split()[2][7], int(ln.split()[2][2:7],16)] for ln in file.splitlines()]
+
+    print("Total part 1: " + str(computeArea(instr)))
+    print("Total part 2: " + str(computeArea(instr_p2)))
+
+def computeArea(instr):
+    cur_x = 0
+    cur_y = 0
+    pointsMap = []
+    len = 0
+    for l in instr:
+        len += l[1]
+        match l[0]:
+            case "3" | "U":
+                cur_y -= l[1]
+            case "0" | "R":
+                cur_x += l[1]
+            case "1" | "D":
+                cur_y += l[1]
+            case "2" | "L":
+                cur_x -= l[1]
+        pointsMap.append([cur_x, cur_y])
+    pointsMap.append([0,0])
+    return int(Polygon(pointsMap).area + (len/2)+1)
+
+main()
